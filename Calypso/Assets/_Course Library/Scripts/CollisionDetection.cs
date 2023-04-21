b2ccc822ad6ed6ffdde56a5ca1fcafe573705257
Veda-Lee <leesky5670@gmail.com>
2023-04-21 14:33:29 -0700

    health value shows up but not if enemy collides

--- Calypso/Assets/_Course Library/Scripts/CollisionDetection.cs
+++ Calypso/Assets/_Course Library/Scripts/CollisionDetection.cs
@@ -7,34 +7,41 @@ public class CollisionDetection : MonoBehaviour
 {
     private int health = 100;
     public TextMeshProUGUI healthText;
+    private Transform player = null;
+    private Transform enemy = null;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        UpdateHealth(health);
     }
 
-void onTriggerEnter(Collider other){
-    if (other.gameObject.tag == "gameOverCollision") {
-       UpdateHealth(-health);
-    }
-}
+//  void onTriggerEnter(Collider other){
+//      if (other.gameObject.tag == "gameOverCollision") {
+// //       UpdateHealth(health);
+//         Debug.Log("collision detected");
+//    }
+//    else{
+//         Debug.Log("No collision detected");
+//    }
+//  }
 
-public void UpdateHealth(int healthToSubtract){
-    health -= healthToSubtract;
-    if (health <= 0) 
-    {
-        
+ public void UpdateHealth(int healthToSubtract){
+    //  if (player.transform.position != null){
+    //     health -= healthToSubtract;
+    //     healthText.text = "Health: " + health;
+    //  }
+    //  else{
         healthText.text = "Health: " + health;
-    
-    }
+    // }
+//     
 }
 
     // Update is called once per frame
     void Update()
     {
      //if (gameObject.tag == "whatIsPlayer".position <= gameObject.tag == "gameOverCollision".position){
-        UpdateHealth(health);
+
     // }
         
     }
